b556671db9e2035a45747fda812b4305ed0c6524
SuperSpawn <thearktitan@gmail.com>
2023-01-08 13:53:03 +0200

    assignment 1.1

---
+++ assignment1.1/main.js
@@ -0,0 +1,16 @@
+
+let string = typeof 'hello world';
+let number = typeof 5;
+let object = typeof number;
+let boolean = typeof false;
+let nil = typeof null;
+let undef;
+let symbol = typeof Symbol("foo");
+
+console.log(string);
+console.log(number);
+console.log(object);
+console.log(boolean);
+console.log(nil);
+console.log(undef);
+console.log(symbol);
