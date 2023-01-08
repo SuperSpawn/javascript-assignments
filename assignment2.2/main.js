2f9c3e8f7dc8e4f4bf17372fd715dd5995492f57
SuperSpawn <thearktitan@gmail.com>
2023-01-08 15:32:57 +0200

    assignment 2.2

---
+++ assignment2.2/main.js
@@ -0,0 +1,12 @@
+
+function tellFortune(jobTitle, location, partnerName, numberOfChildren) {
+    return `You will be a ${jobTitle} in ${location} married to ${partnerName} with ${numberOfChildren} children`;
+}
+
+console.log(tellFortune('Programmer', 'Haifa', 'Fatima', 5));
+
+
+
+
+
+
