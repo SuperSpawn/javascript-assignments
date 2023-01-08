2f29a963007548f39f33c2518d52dc8f42845b6a
SuperSpawn <thearktitan@gmail.com>
2023-01-08 16:18:28 +0200

    assignment 2.3

---
+++ assignment2.3/main.js
@@ -0,0 +1,10 @@
+
+function calculateArea(radius) {
+    console.log(`${Math.PI} * ${radius} * ${radius}`);
+    return (Math.PI * radius * radius).toFixed(2);
+}
+
+console.log(calculateArea(5));
+
+
+
