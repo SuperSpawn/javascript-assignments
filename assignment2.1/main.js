10770b8f2c00023a66cc9db78605be3b7626d0b5
SuperSpawn <thearktitan@gmail.com>
2023-01-08 15:21:44 +0200

    assignment 2.1

---
+++ assignment2.1/main.js
@@ -0,0 +1,11 @@
+
+function countryInfo(country, population, capital) {
+    return `${country} has ${population} people and its capital city is ${capital}`;
+}
+
+console.log(countryInfo("Germany", "83.2 million", "Berlin"));
+console.log(countryInfo("Morroco", "37.08 million", "Rabat"));
+console.log(countryInfo("Canada", "38.25 million", "Ottawa"));
+
+
+
