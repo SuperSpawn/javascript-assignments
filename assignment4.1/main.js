f9f3c7a2e2e66414348e1ad96dd4af6567bf9b5d
SuperSpawn <thearktitan@gmail.com>
2023-01-10 10:31:06 +0200

    assignment4.1

---
+++ assignment4.1/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+let numSiblings = prompt("How many siblings do you have?", "0");
+if(numSiblings == 1) {
+    console.log("1 sibling!");
+}
+else if(numSiblings > 1) {
+    console.log("More than 1 sibling");
+}
+else {
+    console.log("No siblings");
+}
+
+
+
