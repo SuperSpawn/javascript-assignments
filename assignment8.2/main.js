c54323cd652828dc095fd90455d8128d84a3486b
SuperSpawn <thearktitan@gmail.com>
2023-01-11 17:01:22 +0200

    assignment8.2

---
+++ assignment8.2/main.js
@@ -0,0 +1,16 @@
+
+
+
+const myCountry = {
+    country: 'Syria',
+    capital: 'Damascus',
+    language: 'Arabic',
+    population: 21.32,
+    neighbors: ['Lebanon', 'Jordan', 'Israel', 'Turkey', 'Iraq'],
+    describe : function() {
+        return `${this.country} has ${this.population} million people, their mother tongue is ${this.language}, they have 
+        ${this.neighbors.length} neighbors and a capital called ${this.capital}`
+    }
+}
+
+
