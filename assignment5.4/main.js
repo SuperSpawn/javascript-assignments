5b85f1ddb8f072ce6f7130dc2364c30596fe23e1
SuperSpawn <thearktitan@gmail.com>
2023-01-11 09:23:17 +0200

    assignment5.4

---
+++ assignment5.4/main.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+function evaluateColor(color) {
+    switch (color) {
+        case 'yellow':
+        case 'pink':
+        case 'orange':
+            return 'light color';
+        case 'blue':
+        case 'purple':
+        case 'brown':
+            return 'dark color';
+        default:
+            return 'other color';
+    }
+}
+
+
+
+
+
+
+
+
+
