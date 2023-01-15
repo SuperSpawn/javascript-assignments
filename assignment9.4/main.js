28e5cc00a18747c9de0ae90c8690420faa0e17e5
SuperSpawn <thearktitan@gmail.com>
2023-01-15 14:53:56 +0200

    9.4

--- assignment9.4/main.js
+++ assignment9.4/main.js
@@ -3,9 +3,16 @@
 
 function createSteps(number) {
     let string = new String();
+    string = '';
+
     for(let i = 1; i <= number; ++i) {
         string = '';
-        for(let j = 1; j <= i; ++j) string += '#';
+        for(let j = 0; j < i; ++j) {
+            string += '#';
+        }
+        for(let k = i; k < number; ++k) {
+            string += ' ';
+        } 
         console.log(string);
     }
 }
