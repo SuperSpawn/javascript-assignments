07530bc8ca97ef19f999bbf81aafdbbcd2103c77
SuperSpawn <thearktitan@gmail.com>
2023-01-18 16:32:43 +0200

    17.2

---
+++ assignment17.2/main.js
@@ -0,0 +1,19 @@
+/*
+    BLOCK1:
+        we declare and execute a function with value 0, and we declare and execute
+            an inner function with value 1, in that function we print the value given 
+            to the outer function (0)
+*/
+
+
+/*
+    BLOCK2:
+        we declare and execute a function, inside the function we change count to 1
+            and print it then we print it again
+*/
+
+
+/*
+    BLOCK3:
+        
+*/
