94ba5f601cfec4abc2068cf6510392d89ec3c442
SuperSpawn <thearktitan@gmail.com>
2023-02-06 09:08:58 +0200

    23.1

---
+++ assignment23.1/script.js
@@ -0,0 +1,22 @@
+
+
+/*
+Question 1: 
+    this will point to the window object, we are referencing this in the global scope
+
+Question 2:
+    this will point to the window object, we are referencing this in an arrow function which does not affect this even inside an object scope
+
+Question 3: 
+    this will point to nyFuncArrow because when calling a function except arrow functions, the this pointer will point to the current function
+
+Question 4: 
+    this will point to the window object, we are referencing this in an arrow function which does not affect this even inside an object scope
+
+Question 5 a:
+    this will point to the window object because it is referenced in an arrow function
+Question 5 b:
+    instead of using the arrow function, use a callback function instead
+
+
+*/
